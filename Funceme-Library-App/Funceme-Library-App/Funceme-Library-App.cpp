6eebc0fce0a4d8a4d5c47863500f1cdcd54affe9
unknown <joaomsa3@gmail.com>
2016-12-07 21:19:40 -0800

    Arquivos do projeto visual studio com codigo para obter email

---
+++ Funceme-Library-App/Funceme-Library-App/Funceme-Library-App.cpp
@@ -0,0 +1,18 @@
+// Funceme-Library-App.cpp : Arquivo para obter os emails para enviar o curriculo.
+//
+
+#include "stdafx.h"
+#include "ProvaDLL.h"
+#include <iostream>
+
+
+int main()
+{
+
+	Prova p {};
+
+	std::cout << p.getEmail();
+
+    return 0;
+}
+
